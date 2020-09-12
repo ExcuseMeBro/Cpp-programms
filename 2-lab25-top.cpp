a12972e033bfde3b1b33628798c1699ddad30799
Arash4567 <shomoyxhacker007@mail.ru>
2020-09-12 11:34:02 +0500

    solve2

---
+++ 2-lab25-top.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double x;
+    cout<<"x = ";
+    cin>>x;
+    cout<<"Natija: "<<x*(log10(M_E)/log10(2))-cos(x/M_PI)+pow((abs(tan(2*x)/(2.6+pow(x,2)+pow(x,3)))),1.0/3)<<endl;
+    return 0;
+}
