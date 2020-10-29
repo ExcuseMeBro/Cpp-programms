c60b30e12a87d8b52159f491514aada3bf65c953
Arash4567 <shomoyxhacker007@mail.ru>
2020-10-29 19:28:42 +0500

    1

---
+++ 1-kurs1-lab9-top.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double A = 2.6, B = 56.6, C = 35, D = 20.3;
+    double Y = 1 / pow(A, 2.) * pow(B / 10, 3.) * pow(C + D, 2.);
+    cout << "Y = " << Y << endl;
+}
