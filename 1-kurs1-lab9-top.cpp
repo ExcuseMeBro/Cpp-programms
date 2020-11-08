930a5d36a0f015e22e133b112bac7fe014705408
Arash4567 <shomoyxhacker007@mail.ru>
2020-11-08 22:03:54 +0500

    2

--- 1-kurs1-lab9-top.cpp
+++ 1-kurs1-lab9-top.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
     double A = 2.6, B = 56.6, C = 35, D = 20.3;
-    double Y = 1 / pow(A, 2.) * pow(B / 10, 3.) * pow(C + D, 2.);
-    cout << "Y = " << Y << endl;
+    double y = 1 / pow(A, 2.) * pow(B / 10, 3.) * pow(C + D, 2.);
+    cout << "Y = " << y << endl;
 }
