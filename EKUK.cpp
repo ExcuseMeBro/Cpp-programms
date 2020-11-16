fa73c12ae263fb9fda57276729591329214b6b93
Arash4567 <shomoyxhacker007@mail.ru>
2020-11-16 21:58:21 +0500

    ekuk

---
+++ EKUK.cpp
@@ -0,0 +1,21 @@
+//EKUK
+#include <iostream>
+using namespace std;
+long long EKUB(long long int a, long long int b)
+{
+    if (b == 0)
+        return a;
+    return EKUB(b, a % b);
+}
+long long EKUK(int a, int b)
+{
+    return (a / EKUB(a, b)) * b;
+}
+int main()
+{
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << "EKUK(" << a << "," << b << ")=" << EKUK(a, b) << endl;
+    return 0;
+}
