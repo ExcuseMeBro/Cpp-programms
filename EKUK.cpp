10c5bf73167e865a250ef593f3dc2fcd458c9997
Arash4567 <shomoyxhacker007@mail.ru>
2020-11-25 09:58:58 +0500

    7-lab14-top

--- EKUK.cpp
+++ EKUK.cpp
@@ -1,4 +1,4 @@
-//EKUK
+//14.Berilgan   a va b sonlarining    EKUKini  topuvchi rekursiv  funksiya tuzing.
 #include <iostream>
 using namespace std;
 long long EKUB(long long int a, long long int b)
