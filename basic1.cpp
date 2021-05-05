065043ac9f0bb1458f541dc9940447d0729b52a9
Arash4567 <shmoyxhacker007@mail.ru>
2021-05-05 13:59:35 +0500

    assembler

---
+++ basic1.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+char s[] = "GeeksforGeeks";
+int main()
+{
+    int a = 2000, b =17;
+}
---
+++ l21.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c,natija;
+    cin >> a >> b >> c;
+    natija = a*(b+2*c)+b;
+    cout<<natija;
+}
